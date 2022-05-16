5fd2dc8158618ea5b3c9d20a047f20753819c8e1
DMG0411 <m2bobko@gmail.com>
2022-05-16 22:25:32 +0300

    Update

---
+++ OOP/OOP_EX_SIMPLE_CLASSES/OOP_EX_1/NumberList.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <cstdio>
+
+void swap(int x, int y)
+{
+	int aux;
+	aux = x;
+	x = y;
+	y = aux;
+}
+
+class NumberList
+{
+	int numbers[10];
+	int count;
+public:
+	void Init(); // count will be 0
+	bool Add(int x); // adds X to the number list and increase the data member count
+					 // if count is bigger or equal to 10, the function will return false
+	void Sort(); // will sort the numbers of the vector
+	void Print(); // will print the current vector
+};
+
+void NumberList::Init()
+{
+	count = 0;
+}
+bool NumberList::Add(int x)
+{
+	if (count >= 10)
+		return false;
+	else
+		return true;
+}
+void NumberList::Sort()
+{
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10 - i - 1; j++)
+			if (numbers[j] > numbers[j + 1])
+				swap(numbers[j], numbers[j + 1]);
+}
+void NumberList::Print()
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", numbers[i]);
+}
